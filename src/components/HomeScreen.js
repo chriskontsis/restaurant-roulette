d670bd1a0b70a254f6a1268478453594da2ff564
chriskontsis <cak0927@gmail.com>
2022-07-09 19:21:45 -0700

    fixed switching between screens

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function HomeScreen() {
+const HomeScreen = () => {
   return (
     <div>HomeScreen</div>
   )
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,55 +1,43 @@
 import React, { Component } from 'react'
-import HomeScreen from './HomeScreen'
-import RandomRest from './RandomRest'
+import HomeScreen from "./HomeScreen"
+import RandomRest from "./RandomRest"
 
-export class Restaurants extends Component {
-        state =  {
-            step: 1,
-            location: "",
-            name: "",
-            description: "",
-            website: ""
-        }
-
-        prevStep = () => {
-                const {step} = this.state;
-                this.setState({step: step + 1})
-        }
-
-        handleLocationInput = (e) => {
-            this.setState({loaction: e.target.value})
-        }
-
-        reRoll = (e) => {
-
-        }
-
-
+export default class Restaurants extends Component {
+    state =  {
+        step: 1,
+        location: "",
+        name: "",
+        description: "",
+        website: ""
+      };
+      
+      prevStep = () => {
+            const {step} = this.state;
+            this.setState({step: step + 1})
+      }
+      
+      handleLocationInput = (e) => {
+        this.setState({loaction: e.target.value})
+      }
+      
+      
+    
   render() {
-      const step = this.state;
-      const values = {location, name, description, webstie};
-      const {location, name, description, webstie} = this.state;
+    const {step} = this.state;
+    const {location, name, description, website} = this.state;
+    const values = {location, name, description, website};
 
-    switch(step) {
-
-        case 1:
-            return (
-                <HomeScreen
-                values = {values}
-                handleLocationInput = {this.handleLocationInput}
-                />
-            )
-        case 2:
-            return (
-                <RandomRest
-                values = {values}
-                reRoll = {this.reRoll}
-                prevStep = {this.prevStep}
-                />
-            )
-
-    }
+        switch(step) {
+            case 1:
+                return (
+                    <HomeScreen/>
+                )
+            case 2:
+                return (
+                    <RandomRest/>
+                )
+            default:
+        }   
   }
 }
 
-export default Restaurants
