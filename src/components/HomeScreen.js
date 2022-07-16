8e0400930d5044d1fa118e879a967379b301c631
chriskontsis <cak0927@gmail.com>
2022-07-15 17:50:56 -0700

    creating home screen

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { logDOM } from '@testing-library/react';
+import '../App.css'
 
 const HomeScreen = () => {
   const [loading,setLoading] = useState(false);
@@ -23,7 +25,24 @@ axios.request(options).then(function (response) {
 });
 
   return (
-    <div>HomeScreen</div>
+    <div id="page-container"> 
+    <div id="img-container">
+    <img id='landing-img' src={require("../imgs/Navigation-amico.png")} alt="" />
+    </div>
+
+    <div id="form-container">
+
+  
+      <form id="container-items">
+        <h1 id="title"> Enter your zipcode <br /></h1>
+        <input id="input-form"
+          type="text"
+         
+        /> <br />
+        <button id="next-button">Next</button>
+      </form>
+    </div>
+  </div>
   )
 }
 
